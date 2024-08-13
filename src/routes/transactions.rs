cc96aed8a8c19771f5cadc5368374ce2f024210c
weldonkipchirchir <weldonkipchirchir23@gmail.com>
2024-08-14 00:41:45 +0300

    add: view, update and delete transactions

--- src/routes/transactions.rs
+++ src/routes/transactions.rs
@@ -1,9 +1,10 @@
-use crate::model::{NewTransaction, Transaction};
+use crate::model::{NewTransaction, UpdateTransaction};
 use crate::repositories::TransactionsRepository;
 use crate::{AuthenticatedUser, DBConnection};
 use rocket::http::Status;
-use rocket::serde::json::Json;
-use rocket::{post, get, response::status::Custom};
+use rocket::serde::json::{self, Json};
+use rocket::{delete, put};
+use rocket::{get, post, response::status::Custom};
 use serde_json::{json, Value};
 
 #[post("/transaction", format = "json", data = "<new_transaction>")]
@@ -44,6 +45,97 @@ pub async fn view_transactions(
             Err(_) => Err(Custom(
                 Status::InternalServerError,
                 json!({"error":"something went wrong"}),
+            )),
+        },
+    )
+    .await
+}
+
+#[get("/transaction/<id>")]
+pub async fn view_transaction(
+    db: DBConnection,
+    _auth: AuthenticatedUser,
+    id: i32,
+) -> Result<Value, Custom<Value>> {
+    db.run(
+        move |c| match TransactionsRepository::find_transaction(c, id) {
+            Ok(Some(transaction)) => Ok(json!(transaction)),
+            Ok(None) => Err(Custom(
+                Status::NotFound,
+                json!({"error":"Transaction not found"}),
+            )),
+            Err(_) => Err(Custom(
+                Status::InternalServerError,
+                json!({"error":"something went wrong"}),
+            )),
+        },
+    )
+    .await
+}
+
+#[put("/transaction/<id>", format = "json", data = "<transaction>")]
+pub async fn update_transaction(
+    db: DBConnection,
+    _auth: AuthenticatedUser,
+    id: i32,
+    transaction: Json<UpdateTransaction>,
+) -> Result<Custom<Value>, Custom<Value>> {
+    db.run(
+        move |c| match TransactionsRepository::find_transaction(c, id) {
+            Ok(Some(_)) => match transaction.validate() {
+                Ok(()) => match TransactionsRepository::update_transaction(
+                    c,
+                    id,
+                    transaction.into_inner(),
+                ) {
+                    Ok(transaction_res) => {
+                        Ok(Custom(Status::Ok, json!({"message":transaction_res})))
+                    }
+                    Err(_) => Err(Custom(
+                        Status::InternalServerError,
+                        json!({"error":"something went wrong"}),
+                    )),
+                },
+                Err(errors) => Err(Custom(Status::BadRequest, json!({"error":errors}))),
+            },
+            Ok(None) => Err(Custom(
+                Status::NotFound,
+                json!({"error":"Transaction not found"}),
+            )),
+            Err(_) => Err(Custom(
+                Status::InternalServerError,
+                json!({"errror":"something went wrong"}),
+            )),
+        },
+    )
+    .await
+}
+
+#[delete("/transaction/<id>")]
+pub async fn delete_transaction(
+    db: DBConnection,
+    _auth: AuthenticatedUser,
+    id: i32,
+) -> Result<Value, Custom<Value>> {
+    db.run(
+        move |c| match TransactionsRepository::find_transaction(c, id) {
+            Ok(Some(_)) => match TransactionsRepository::delete_transaction(c, id) {
+                Ok(_) => Ok(json!({"message":"transaction deleted"})),
+                Err(err) => {
+                    eprintln!("Error fetching budget: {:?}", err);
+                    Err(Custom(
+                        Status::InternalServerError,
+                        json!("Something went wrong"),
+                    ))
+                }
+            },
+            Ok(None) => Err(Custom(
+                Status::NotFound,
+                json!({"error": "transaction not found"}),
+            )),
+            Err(_) => Err(Custom(
+                Status::InternalServerError,
+                json!({"error":"Something went wrong"}),
             )),
         },
     )
