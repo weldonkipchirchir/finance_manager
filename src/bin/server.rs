2d674f75c8ee4fcaa499b014ec4c9126f076494a
weldonkipchirchir <weldonkipchirchir23@gmail.com>
2024-08-14 00:45:01 +0300

    add: view, update and delete transactions routes

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,14 +2,14 @@ extern crate finance_manager;
 use std::str::FromStr;
 
 use rocket::{self, routes};
-use rocket_cors::{CorsOptions, AllowedHeaders, AllowedOrigins, AllowedMethods};
+use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins, CorsOptions};
 
 #[rocket::main]
 async fn main() {
-    let allowed_methods : AllowedMethods = ["Get", "Post", "Put", "Delete"]
-    .iter()
-    .map(|s| FromStr::from_str(s).unwrap())
-    .collect();
+    let allowed_methods: AllowedMethods = ["Get", "Post", "Put", "Delete"]
+        .iter()
+        .map(|s| FromStr::from_str(s).unwrap())
+        .collect();
 
     let cors = CorsOptions {
         allowed_origins: AllowedOrigins::some_exact(&["http://localhost:3000"]),
@@ -27,6 +27,7 @@ async fn main() {
             routes![
                 finance_manager::routes::user::create_user,
                 finance_manager::routes::user::login,
+                finance_manager::routes::user::update_user,
                 finance_manager::routes::budget::create_budget,
                 finance_manager::routes::budget::update_budget,
                 finance_manager::routes::budget::view_budget,
@@ -34,9 +35,9 @@ async fn main() {
                 finance_manager::routes::budget::view_budgets,
                 finance_manager::routes::transactions::create_transaction,
                 finance_manager::routes::transactions::view_transactions,
-                // finance_manager::routes::transactions::view_transaction,
-                // finance_manager::routes::transactions::update_transaction,
-                // finance_manager::routes::transactions::delete_transaction
+                finance_manager::routes::transactions::view_transaction,
+                finance_manager::routes::transactions::update_transaction,
+                finance_manager::routes::transactions::delete_transaction
             ],
         )
         .attach(finance_manager::DBConnection::fairing())
