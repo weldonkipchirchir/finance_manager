9416a0a00ee25aad03f629778c87ac30f6a5461b
weldonkipchirchir <weldonkipchirchir23@gmail.com>
2024-08-20 19:59:05 +0300

    add: savings column to goals table

---
+++ migrations/2024-08-18-231505_add_saving_to_goals/down.sql
@@ -0,0 +1,2 @@
+-- This file should undo anything in `up.sql`
+ALTER TABLE goals DROP COLUMN saving;
---
+++ migrations/2024-08-18-231505_add_saving_to_goals/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+ALTER TABLE goals ADD COLUMN saving DECIMAL(10, 2) DEFAULT 0;
